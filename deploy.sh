52f6bc71659faaec428c428805856373e026b5e8
tagty <bouzu.ao@gmail.com>
2020-08-23 22:33:20 +0900

    mysql

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo 'deploy to isucon1'
-ssh isucon1 "cd /home/isucon/isucari && git pull && cd /home/isucon/isucari/webapp/ruby && /home/isucon/local/ruby/bin/bundle install --path=.bundle && sudo systemctl restart mysql.service && sudo systemctl restart isucari.ruby.service && sudo systemctl restart nginx.service && sudo sysctl -p"
+ssh isucon1 "export MYSQL_HOST=ec2-52-69-203-123.ap-northeast-1.compute.amazonaws.com && cd /home/isucon/isucari && git pull && cd /home/isucon/isucari/webapp/ruby && /home/isucon/local/ruby/bin/bundle install --path=.bundle && sudo systemctl restart mysql.service && sudo systemctl restart isucari.ruby.service && sudo systemctl restart nginx.service && sudo sysctl -p"
 echo 'deploy done'
 
 echo 'deploy to isucon2'
