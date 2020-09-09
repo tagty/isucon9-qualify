b3e47235a006fbc5fd1b956bbb9747fe860bd0e7
tagty <bouzu.ao@gmail.com>
2020-09-09 21:57:25 +0900

    add

---
+++ Vagrantfile
@@ -0,0 +1,4 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "matsuu/isucon9-qualifier"
+  config.vm.box_version = "0.1.20200710"
+end
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -ex
 
-echo 'deploy to isucon1'
-ssh isucon1 "cd /home/isucon/isucari && git pull && cd /home/isucon/isucari/webapp/ruby && /home/isucon/local/ruby/bin/bundle install --path=.bundle && sudo systemctl restart mysql.service && sudo systemctl restart isucari.ruby.service && sudo systemctl restart nginx.service && sudo sysctl -p"
-echo 'deploy done'
+BRANCH='master'
 
-echo 'deploy to isucon2'
-ssh isucon2 "cd /home/isucon/isucari && git pull && cd /home/isucon/isucari/webapp/ruby && /home/isucon/local/ruby/bin/bundle install --path=.bundle && sudo systemctl restart mysql.service && sudo systemctl restart isucari.ruby.service && sudo systemctl restart nginx.service && sudo sysctl -p"
+# echo 'deploy to isucon1'
+# ssh isucon1 "cd /home/isucon/isucari && git pull && cd /home/isucon/isucari/webapp/ruby && /home/isucon/local/ruby/bin/bundle install --path=.bundle && sudo systemctl restart mysql.service && sudo systemctl restart isucari.ruby.service && sudo systemctl restart nginx.service && sudo sysctl -p"
+# echo 'deploy done'
+
+# echo 'deploy to isucon2'
+# ssh isucon2 "cd /home/isucon/isucari && git pull && cd /home/isucon/isucari/webapp/ruby && /home/isucon/local/ruby/bin/bundle install --path=.bundle && sudo systemctl restart mysql.service && sudo systemctl restart isucari.ruby.service && sudo systemctl restart nginx.service && sudo sysctl -p"
+# echo 'deploy done'
+
+echo 'deploy to isucon1'
+ssh isucon1 "cd /home/isucon/isucari && git fetch && git checkout ${BRANCH} && git pull && cd /home/isucon/isucari/webapp/ruby && /home/isucon/local/ruby/bin/bundle install --path=.bundle && sudo systemctl restart mysql.service && sudo systemctl restart isucari.ruby.service && sudo systemctl restart nginx.service && sudo sysctl -p"
 echo 'deploy done'
